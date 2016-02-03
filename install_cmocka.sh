23834b5fd3dc6dde171369483c5421819807954f
Louis Taylor <louis@kragniz.eu>
2016-02-03 19:28:30 +0000

    Install cmocka in home directory

--- install_cmocka.sh
+++ install_cmocka.sh
@@ -5,7 +5,7 @@ tar vxf cmocka-1.0.1.tar.xz
 cd cmocka-1.0.1/
 mkdir build
 cd build/
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_INSTALL_PREFIX=~ -DCMAKE_BUILD_TYPE=Release ..
 make
 make install
 
