021250a53df4f5d8c4a5e06ff9a89bb31f0f4325
Louis Taylor <louis@kragniz.eu>
2016-03-01 01:07:47 +0000

    Add sailing_ prefix

--- sailing.c
+++ sailing.c
@@ -3,8 +3,8 @@
 
 #include "sailing.h"
 
-struct boat *sailing_boat_init(const char *name) {
-    struct boat *boat = calloc(1, sizeof(struct boat));
+struct sailing_boat *sailing_boat_init(const char *name) {
+    struct sailing_boat *boat = calloc(1, sizeof(struct sailing_boat));
     boat->name = strdup(name);
     boat->longitude = 0;
     boat->latitude = 0;
@@ -12,19 +12,19 @@ struct boat *sailing_boat_init(const char *name) {
     return boat;
 }
 
-void sailing_boat_free(struct boat *boat) {
+void sailing_boat_free(struct sailing_boat *boat) {
     free(boat->name);
     free(boat);
 }
 
-char *sailing_boat_get_name(struct boat *boat) {
+char *sailing_boat_get_name(struct sailing_boat *boat) {
   return boat->name;
 }
 
-double sailing_boat_get_latitude(struct boat *boat) {
+double sailing_boat_get_latitude(struct sailing_boat *boat) {
     return boat->latitude;
 }
 
-double sailing_boat_get_longitude(struct boat *boat) {
+double sailing_boat_get_longitude(struct sailing_boat *boat) {
     return boat->longitude;
 }
--- sailing.h
+++ sailing.h
@@ -1,20 +1,20 @@
 #ifndef SAILING_H
 #define SAILING_H
 
-struct boat {
+struct sailing_boat {
     char *name;
     double longitude;
     double latitude;
 };
 
-struct boat *sailing_boat_init();
+struct sailing_boat *sailing_boat_init();
 
-void sailing_boat_free(struct boat *boat);
+void sailing_boat_free(struct sailing_boat *boat);
 
-char *sailing_boat_get_name(struct boat *boat);
+char *sailing_boat_get_name(struct sailing_boat *boat);
 
-double sailing_boat_get_latitude(struct boat *boat);
+double sailing_boat_get_latitude(struct sailing_boat *boat);
 
-double sailing_boat_get_longitude(struct boat *boat);
+double sailing_boat_get_longitude(struct sailing_boat *boat);
 
 #endif
