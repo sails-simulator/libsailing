605233c7c995cc07cebe0a329a52c974d11c7275
Louis Taylor <louis@kragniz.eu>
2018-10-05 18:58:45 +0100

    Bump cmocka version

--- testing/install_cmocka.sh
+++ testing/install_cmocka.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-wget https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz
-tar vxf cmocka-1.0.1.tar.xz
-cd cmocka-1.0.1/
+wget https://cmocka.org/files/1.1/cmocka-1.1.3.tar.xz
+tar vxf cmocka-1.1.3.tar.xz
+cd cmocka-1.1.3/
 mkdir build
 cd build/
 cmake -DCMAKE_INSTALL_PREFIX=~ -DCMAKE_BUILD_TYPE=Release ..
